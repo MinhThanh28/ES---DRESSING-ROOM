9217fb24d7a4403231130dcdeb898928daa6eb5a
Minh Thành Lê <leminhthanh.it2810@gmail.com>
2024-01-07 01:14:25 +0700

    update

---
+++ controllers/main.js
@@ -0,0 +1,280 @@
+const ao = [
+    {
+      id: "ao1",
+      image: "../assets/images/clothes/topcloth1.png",
+      imageShowinTab: "../assets/images/clothes/topcloth1_show.jpg",
+      name: "top cloth 1",
+      class: "bikinitop",
+    },
+    {
+      id: "ao2",
+      image: "../assets/images/clothes/topcloth2.png",
+      imageShowinTab: "../assets/images/clothes/topcloth2_show.jpg",
+      name: "top cloth 2",
+      class: "bikinitop",
+    },
+    {
+      id: "ao3",
+      image: "../assets/images/clothes/topcloth3.png",
+      imageShowinTab: "../assets/images/clothes/topcloth3_show.jpg",
+      name: "top cloth 3",
+      class: "bikinitop",
+    },
+    {
+      id: "ao4",
+      image: "../assets/images/clothes/topcloth4.png",
+      imageShowinTab: "../assets/images/clothes/topcloth4_show.jpg",
+      name: "top cloth 4",
+      class: "bikinitop",
+    },
+    {
+      id: "ao5",
+      image: "../assets/images/clothes/topcloth5.png",
+      imageShowinTab: "../assets/images/clothes/topcloth5_show.jpg",
+      name: "top cloth 5",
+      class: "bikinitop",
+    },
+    {
+      id: "ao6",
+      image: "../assets/images/clothes/topcloth6.png",
+      imageShowinTab: "../assets/images/clothes/topcloth6_show.jpg",
+      name: "top cloth 6",
+      class: "bikinitop",
+    },
+  ];
+  const quan = [
+    {
+      id: "quan1",
+      image: "../assets/images/clothes/botcloth1.png",
+      imageShowinTab: "../assets/images/clothes/botcloth1_show.jpg",
+      name: "bottom cloth 1",
+      class: "bikinibottom",
+    },
+    {
+      id: "quan2",
+      image: "../assets/images/clothes/botcloth2.png",
+      imageShowinTab: "../assets/images/clothes/botcloth2_show.jpg",
+      name: "bottom cloth 2",
+      class: "bikinibottom",
+    },
+    {
+      id: "quan3",
+      image: "../assets/images/clothes/botcloth3.png",
+      imageShowinTab: "../assets/images/clothes/botcloth3_show.jpg",
+      name: "bottom cloth 3",
+      class: "bikinibottom",
+    },
+    {
+      id: "quan4",
+      image: "../assets/images/clothes/botcloth4.png",
+      imageShowinTab: "../assets/images/clothes/botcloth4_show.jpg",
+      name: "bottom cloth 4",
+      class: "bikinibottom",
+    },
+    {
+      id: "quan5",
+      image: "../assets/images/clothes/botcloth5.png",
+      imageShowinTab: "../assets/images/clothes/botcloth5_show.jpg",
+      name: "bottom cloth 5",
+      class: "bikinibottom",
+    },
+  ];
+  const giaydep = [
+    {
+      id: "giaydep1",
+      image: "../assets/images/shoes/shoes1.png",
+      imageShowinTab: "../assets/images/shoes/shoes1_show.jpg",
+      name: "shoes 1",
+      class: "feet",
+    },
+    {
+      id: "giaydep2",
+      image: "../assets/images/shoes/shoes2.png",
+      imageShowinTab: "../assets/images/shoes/shoes2_show.jpg",
+      name: "shoes 2",
+      class: "feet",
+    },
+    {
+      id: "giaydep3",
+      image: "../assets/images/shoes/shoes3.png",
+      imageShowinTab: "../assets/images/shoes/shoes3_show.jpg",
+      name: "shoes 3",
+      class: "feet",
+    },
+    {
+      id: "giaydep4",
+      image: "../assets/images/shoes/shoes4.png",
+      imageShowinTab: "../assets/images/shoes/shoes4_show.jpg",
+      name: "shoes 4",
+      class: "feet",
+    },
+    {
+      id: "giaydep5",
+      image: "../assets/images/shoes/shoes5.png",
+      imageShowinTab: "../assets/images/shoes/shoes5_show.jpg",
+      name: "shoes 5",
+      class: "feet",
+    },
+  ];
+  const tuixach = [
+    {
+      id: "tuixach1",
+      image: "../assets/images/handbags/handbag1.png",
+      imageShowinTab: "../assets/images/handbags/handbag1_show.jpg",
+      name: "handbag 1",
+      class: "handbag",
+    },
+    {
+      id: "tuixach2",
+      image: "../assets/images/handbags/handbag2.png",
+      imageShowinTab: "../assets/images/handbags/handbag2_show.jpg",
+      name: "handbag 2",
+      class: "handbag",
+    },
+    {
+      id: "tuixach3",
+      image: "../assets/images/handbags/handbag3.png",
+      imageShowinTab: "../assets/images/handbags/handbag3_show.jpg",
+      name: "handbag 3",
+      class: "handbag",
+    },
+  ];
+  const daychuyen = [
+    {
+      id: "daychuyen1",
+      image: "../assets/images/necklaces/necklace1.png",
+      imageShowinTab: "../assets/images/necklaces/necklace1_show.jpg",
+      name: "necklace 1",
+      class: "necklace",
+    },
+    {
+      id: "daychuyen2",
+      image: "../assets/images/necklaces/necklace2.png",
+      imageShowinTab: "../assets/images/necklaces/necklace2_show.jpg",
+      name: "necklace 2",
+      class: "necklace",
+    },
+    {
+      id: "daychuyen3",
+      image: "../assets/images/necklaces/necklace3.png",
+      imageShowinTab: "../assets/images/necklaces/necklace3_show.jpg",
+      name: "necklace 3",
+      class: "necklace",
+    },
+  ];
+  const kieutoc = [
+    {
+      id: "kieutoc1",
+      image: "../assets/images/hairstyle/hairstyle1.png",
+      imageShowinTab: "../assets/images/hairstyle/hairstyle1_show.jpg",
+      name: "hairstyle 1",
+      class: "hairstyle",
+    },
+    {
+      id: "kieutoc2",
+      image: "../assets/images/hairstyle/hairstyle2.png",
+      imageShowinTab: "../assets/images/hairstyle/hairstyle2_show.jpg",
+      name: "hairstyle 2",
+      class: "hairstyle",
+    },
+    {
+      id: "kieutoc3",
+      image: "../assets/images/hairstyle/hairstyle3.png",
+      imageShowinTab: "../assets/images/hairstyle/hairstyle3_show.jpg",
+      name: "hairstyle 3",
+      class: "hairstyle",
+    },
+  ];
+  const nen =[
+      {
+          id: "nen1",
+          image: "../assets/images/background/background1.jpg",
+          imageShowinTab: "../assets/images/background/background1_show.jpg",
+          name: "background 1",
+          class: "background",
+      },
+      {
+          id: "nen2",
+          image: "../assets/images/background/background2.jpg",
+          imageShowinTab: "../assets/images/background/background2_show.jpg",
+          name: "background 2",
+          class: "background",
+      },
+      {
+          id: "nen3",
+          image: "../assets/images/background/background3.jpg",
+          imageShowinTab: "../assets/images/background/background3_show.jpg",
+          name: "background 3",
+          class: "background",
+      },
+      {
+          id: "nen4",
+          image: "../assets/images/background/background4.jpg",
+          imageShowinTab: "../assets/images/background/background4_show.jpg",
+          name: "background 4",
+          class: "background",
+      },
+      {
+          id: "nen5",
+          image: "../assets/images/background/background5.jpg",
+          imageShowinTab: "../assets/images/background/background5_show.jpg",
+          name: "background 5",
+          class: "background",
+      },
+      {
+          id: "nen6",
+          image: "../assets/images/background/background6.jpg",
+          imageShowinTab: "../assets/images/background/background6_show.jpg",
+          name: "background 6",
+          class: "background",
+      },
+      {
+          id: "nen7",
+          image: "../assets/images/background/background7.jpg",
+          imageShowinTab: "../assets/images/background/background7_show.jpg",
+          name: "background 7",
+          class: "background",
+      },
+     
+  ]
+  function addProductToTab(items, tabId) {
+    const container = document.getElementById(tabId);
+    container.innerHTML = '';
+    items.forEach((item) => {
+      const html = `
+          <div class="card">
+              <img
+                  src="${item.imageShowinTab}"
+                  alt="Tên ảnh"
+                  class="card-img-top"
+                  style="width: 200px; height: 250px"
+              />
+              <div class="card-body">
+                  <h5
+                      class="card-title"
+                      style="font-size: 15px; text-align: center"
+                  >
+                      ${item.name}
+                  </h5>
+                  <button class="btn btn-success" onclick='tryClothes(${JSON.stringify(
+                    item
+                  )})'>Thử đồ</button>
+              </div>
+          </div>
+      `;
+      container.innerHTML += html;
+    });
+  }
+  function tryClothes(item) {
+    const bikiniTop = document.querySelector(`.${item["class"]}`);
+    bikiniTop.innerHTML = `<img style="position: absolute; width: 250px; height: 400px;" src="${item.image}" alt="">`;
+  }
+  
+  addProductToTab(ao, "ao");
+  addProductToTab(quan, "quan");
+  addProductToTab(giaydep, "giaydep");
+  addProductToTab(tuixach, "tuixach");
+  addProductToTab(daychuyen, "daychuyen");
+  addProductToTab(kieutoc, "kieutoc");
+  addProductToTab(nen, "nen");
+  
